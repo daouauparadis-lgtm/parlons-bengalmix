7a186abe9772284f1b01a3cd7d9c05406194bfbc
daouauparadis-lgtm <daouauparadis@gmail.com>
2025-12-17 09:44:08 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "bengalmix-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/lang/fr.json",
+  "/lang/en.json",
+  "/lang/bn.json"
+];
+
+// Installation du service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation et nettoyage des anciens caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
